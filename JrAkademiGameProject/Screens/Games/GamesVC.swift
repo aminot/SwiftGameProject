ec78b10ecd057ce1eedcd48b92a26aacf2100dcb
aminot <ufukdnmz13@gmail.com>
2023-05-31 15:16:36 +0300

    testttt

--- JrAkademiGameProject/Screens/Games/GamesVC.swift
+++ JrAkademiGameProject/Screens/Games/GamesVC.swift
@@ -1,117 +1,68 @@
 import UIKit
 import SnapKit
+import Carbon
+
+
 
 class GamesVc: UIViewController, UISearchBarDelegate {
-    var gamesViewModel: GamesViewModel? = GamesViewModel()
+
     private let tableView = UITableView()
     private let cellIdentifier = "Cell"
-    var games: [GameModel] = []
-    
+
+    var isToggled = false {
+        didSet { render() }
+    }
+
+    private let renderer = Renderer(
+        adapter: UITableViewAdapter(),
+        updater: UITableViewUpdater()
+    )
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        gamesViewModel?.fetchGames(completion: { [weak self] in
-            // Veri çekme tamamlandığında closure içinde güncelleme yap
-            if let gamesViewModel = self?.gamesViewModel {
-                self?.games = gamesViewModel.games
 
-                DispatchQueue.main.async {
-                    // TableView'ı güncelle
-                    self?.tableView.reloadData()
-                }
-            }
-        })
+        title = "Hello"
+        tableView.contentInset.top = 44
+        tableView.separatorStyle = .none
+        renderer.target = tableView
 
         setupUI()
+        render()
     }
-    
+
+    func render() {
+        var sections: [Section] = []
+
+        // Create a cell item containing the HelloMessage view
+        let helloCell = CellNode(id: "hello", HelloMessage(name: "ufuk", surname: "dönmez"))
+        let spacerCell = CellNode(id: "spacer", SpacerComponent(250)) // 10 bir boşluk yüksekliği
+        let helloCell2 = CellNode(id: "hello", HelloMessage(name: "saaaa", surname: "saaa"))
+
+        // Convert helloCell2 into a ViewNode
+        let helloCell2ViewNode = ViewNode(HelloMessage(name: "fffff", surname: "fffff"))
+
+        // Create a section and assign the cell items and footer to it
+        let helloSection = Section(id: "hello", cells: [helloCell, spacerCell], footer: helloCell2ViewNode)
+
+        sections.append(helloSection)
+
+        renderer.render(sections)
+    }
+
+
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesEnded(touches, with: event)
+        isToggled.toggle()
+    }
+
     private func setupUI() {
-        // Bottom Tab Navigation
-        let tabBarController = UITabBarController()
-        let gameViewController = UIViewController()
-        let gameLabel = UILabel()
-        gameViewController.tabBarItem = UITabBarItem(title: "Games", image: UIImage(named: "Vector"), selectedImage: UIImage(named: "aaaSelectedImage"))
-        let favarotiesViewController = UIViewController()
-        favarotiesViewController.view.backgroundColor = .white
-        favarotiesViewController.title = "bbb"
-        favarotiesViewController.tabBarItem = UITabBarItem(title: "Favorites", image: UIImage(named: "Icon"), selectedImage: UIImage(named: "bbbSelectedImage"))
-        tabBarController.viewControllers = [gameViewController, favarotiesViewController]
-        addChild(tabBarController)
-        view.addSubview(tabBarController.view)
-        tabBarController.didMove(toParent: self)
-        
-        // GAMES yazısı sadece gameViewController'a eklenecek
-        let gamesLabel = UILabel()
-        gamesLabel.text = "GAMES"
-        gamesLabel.textAlignment = .left
-        gamesLabel.font = UIFont(name: "Roboto", size: 34)
-        gamesLabel.textColor = UIColor(named: "#000000")
-        gameViewController.view.addSubview(gamesLabel)
-        
-        let boldFontDescriptor = gamesLabel.font.fontDescriptor.withSymbolicTraits(.traitBold)
-        let boldFont = UIFont(descriptor: boldFontDescriptor!, size: gamesLabel.font.pointSize)
-        gamesLabel.font = boldFont
-        
-        gamesLabel.snp.makeConstraints { make in
-            make.leading.equalTo(gameViewController.view).offset(16)
-            make.top.equalTo(gameViewController.view).offset(90)
-            make.width.equalTo(109)
-            make.height.equalTo(41)
-        }
-        
-        // Search Bar
-        let searchBar = UISearchBar()
-        searchBar.placeholder = "Search for the games"
-        gameViewController.view.addSubview(searchBar)
-        
-        searchBar.snp.makeConstraints { make in
-            make.leading.equalTo(gameViewController.view).offset(16)
-            make.top.equalTo(gamesLabel.snp.bottom).offset(9)
-            make.trailing.equalTo(gameViewController.view).offset(-16)
-        }
-        
-        searchBar.delegate = self
-        
-        // Table View
-        tableView.delegate = self
-        tableView.dataSource = self
-        tableView.translatesAutoresizingMaskIntoConstraints = false
-        // Hücre kimliği için GameTableViewCell sınıfını kaydet
-        tableView.register(GameTableViewCell.self, forCellReuseIdentifier: cellIdentifier)
-        tableView.backgroundColor = UIColor(red: 224/255, green: 224/255, blue: 224/255, alpha: 1.0)
-        gameViewController.view.addSubview(tableView)
-        
-        tableView.snp.makeConstraints { make in
-            make.top.equalTo(searchBar.snp.bottom).offset(8)
-            make.leading.trailing.equalTo(gameViewController.view)
-            make.bottom.equalTo(tabBarController.tabBar.snp.top)
-        }
-        
-        tableView.register(UITableViewCell.self, forCellReuseIdentifier: cellIdentifier)
+        // TableView ve diğer arayüz bileşenlerini burada yapılandırabilirsiniz.
+        view.addSubview(tableView) // TableView'ı ekranın görünür kısmına ekleyin
+        tableView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+                }
+        tableView.backgroundColor = UIColor.red
     }
-    
+
     // MARK: - UISearchBarDelegate
-    
-    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
-        // Arama çubuğunda metin değiştiğinde yapılacak işlemler
-        print("Arama çubuğunda yeni metin: \(searchText)")
-    }
-}
-
-extension GamesVc: UITableViewDelegate, UITableViewDataSource {
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return games.count
-    }
-
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath) as? GameTableViewCell ?? GameTableViewCell(style: .default, reuseIdentifier: cellIdentifier)
-        
-        // Veriyi al
-        let game = games[indexPath.row]
-        
-        // Hücreyi yapılandır
-        cell.configure(with: game)
-        
-        // Yapılandırılmış hücreyi döndür
-        return cell
-    }
 }
---
+++ JrAkademiGameProject/UIComponents/Components/SpacerComponent.swift
@@ -0,0 +1,53 @@
+//
+//  SpacingComponent.swift
+//  HBScenes
+//
+//  Created by yusuf terzi on 2.03.2023.
+//
+
+import UIKit
+import Carbon
+
+public struct SpacerComponent: Component {
+  
+  private let height: CGFloat
+  private let color: UIColor
+  private let forceHeight: Bool
+
+  public init(
+    _ height: CGFloat,
+    color: UIColor = .clear,
+    forceHeight: Bool = false
+  ) {
+    self.height = height
+    self.color = color
+    self.forceHeight = forceHeight
+  }
+
+  public func shouldContentUpdate(with next: SpacerComponent) -> Bool {
+    true
+  }
+  
+  public func renderContent() -> UIView {
+    let view = UIView()
+    view.backgroundColor = color
+    if forceHeight {
+      view.snp.makeConstraints {
+        $0.height.equalTo(height)
+      }
+    }
+    return view
+  }
+
+  public func render(in content: UIView) {
+    content.backgroundColor = color
+  }
+
+  public func shouldRender(next: SpacerComponent, in content: UIView) -> Bool {
+    return true
+  }
+
+  public func referenceSize(in bounds: CGRect) -> CGSize? {
+    CGSize(width: bounds.width, height: height)
+  }
+}
